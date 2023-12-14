7591716573f05bafaf060ba4a464d7c5379416ef
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2023-12-14 19:49:43 +0200

    first commit

---
+++ src/views/AboutView.vue
@@ -0,0 +1,7 @@
+<template>
+  <div class="bg-red-100">
+    <h1>This is an about page</h1>
+  </div>
+</template>
+
+<style></style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <main>
+    <h1 class="text-3xl font-bold underline">Hello world!</h1>
+  </main>
+</template>
