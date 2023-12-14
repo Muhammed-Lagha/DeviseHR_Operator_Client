9f7a0cd1884487c09a921bb76c8430e325ea1582
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2023-12-14 23:10:34 +0200

    Init NavBar

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,22 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
+import { RouterLink, RouterView } from 'vue-router';
+import TopNav from "@/components/layout/Navigation/TopNav.vue";
+
 </script>
 
 <template>
   <header>
-    <div class="flex justify-center items-center ">
+    <TopNav />
+    <!-- <div class="flex justify-center items-center ">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
-    </div>
+    </div> -->
+    
   </header>
 
   <RouterView />
 </template>
 
-<style scoped>
-</style>
+<style scoped></style>
