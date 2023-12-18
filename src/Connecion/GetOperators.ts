d8529ebe2341298c6059f25ff8e6b6f21c0fd72c
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2023-12-18 20:56:18 +0200

    init a connection string

---
+++ src/Connecion/GetOperators.ts
@@ -0,0 +1,14 @@
+import axios from "axios";
+import { opApiConnection, token } from '@/Connecion/ConnectionStrings';
+
+export const getOperators = async () => {
+    const response = await axios.get(
+        `${opApiConnection}/api/admin-operators/operators`,
+        {
+          headers: {
+            'Content-Type': 'application/json',
+            Authorization: `Bearer ${token}`,
+          },
+        },);
+    return response.data.data
+}
