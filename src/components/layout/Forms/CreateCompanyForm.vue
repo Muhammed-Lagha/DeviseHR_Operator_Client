991f68f5a6b1a7524a9e0c85b3c0fa2bbca59cbd
Muhammed-Lagha <lagha.muhammed@yahoo.com>
2024-01-08 23:52:08 +0200

    create company Api

--- src/components/layout/Forms/CreateCompanyForm.vue
+++ src/components/layout/Forms/CreateCompanyForm.vue
@@ -4,48 +4,40 @@ import PencilIcon from '@/components/icons/other/PencilIcon.vue'
 import PhoneIcon from '@/components/icons/other/PhoneIcon.vue'
 import LocationIcon from '@/components/icons/other/LocationIcon.vue'
 import EmailIcon from '@/components/icons/other/EmailIcon.vue'
-import UserRole from '@/components/icons/other/UserRole.vue'
 import UserIcon from '@/components/icons/other/UsersIcon.vue'
 import CalendarIcon from '@/components/icons/other/CalendarIcon.vue'
 import EditNameIcon from '@/components/icons/other/EditNameIcon.vue'
-import MailIcon from '@/components/icons/other/MailIcon.vue'
-interface Company {
-  companyName: string
-  companyEmail: string
-  firstName: string
-  lastName: string
-  phoneNumber: string
-  clientEmail: string
-  userRole: string
-  country: string
-  expirationDate: string
-  isSpecialClient: boolean
-  maxEmployeesAllowed: number
-  licenceNumber: string
-  accountNumber: string
-  sendRegistration: boolean
-}
+import { createCompany } from '@/Api/createCompanyApi'
+import { getAuthToken } from '@/utils/getTokens'
 
-const company = ref<Company>({
-  companyName: '',
-  companyEmail: '',
-  clientEmail: '',
-  userRole: 'admin',
-  firstName: '',
-  lastName: '',
-  phoneNumber: '',
-  country: '',
-  expirationDate: '',
-  isSpecialClient: false,
-  maxEmployeesAllowed: 0,
-  licenceNumber: '',
-  accountNumber: '',
-  sendRegistration: false
-})
+const companyName = ref('')
+const firstName = ref('')
+const lastName = ref('')
+const email = ref('')
+const expirationDate = ref('')
+const phoneNumber = ref('')
+const maxEmployeesAllowed = ref(0)
+const licenceNumber = ref('')
+const accountNumber = ref('')
+const sendRegistration = ref(false)
 
-const register = () => {
-  const data = JSON.parse(JSON.stringify(company.value))
-  console.log('Registered: ', data)
+const token = getAuthToken()
+
+const register = async () => {
+  if (token === null) throw new Error('no token')
+  await createCompany(
+    token,
+    companyName.value,
+    firstName.value,
+    lastName.value,
+    email.value,
+    expirationDate.value,
+    phoneNumber.value,
+    Number(maxEmployeesAllowed.value),
+    licenceNumber.value,
+    accountNumber.value,
+    sendRegistration.value
+  )
 }
 </script>
 
@@ -56,8 +48,8 @@ const register = () => {
         <h3 class="text-sm text-indigo-600">Create New Company</h3>
       </div>
       <div class="flex flex-wrap">
-        <!-- first name -->
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
+        <!-- Company name -->
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
           <label class="text-xs">Company Name</label>
           <div class="relative mt-2 rounded-md shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
@@ -66,28 +58,13 @@ const register = () => {
 
             <input
               type="text"
-              v-model="company.companyName"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
-            />
-          </div>
-        </div>
-        <!-- Email -->
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
-          <label class="text-xs">Company Email</label>
-          <div class="relative mt-2 rounded-md shadow-sm">
-            <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
-              <EmailIcon class="w-6 h-6" />
-            </span>
-
-            <input
-              type="text"
-              v-model="company.companyEmail"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+              v-model="companyName"
+              class="w-full px-12 py-2 border-transparent rounded-md shadow-md appearance-none input"
             />
           </div>
         </div>
         <!-- Phone -->
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
           <label class="text-xs">Company Phone</label>
           <div class="relative mt-2 rounded-md shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
@@ -95,15 +72,15 @@ const register = () => {
             </span>
 
             <input
-              type="number"
-              v-model="company.phoneNumber"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+              type="text"
+              v-model="phoneNumber"
+              class="w-full px-12 py-2 border-transparent rounded-md shadow-md appearance-none input"
             />
           </div>
         </div>
-        <!-- User Role -->
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
-          <label class="text-xs">Company Location</label>
+        <!-- Licence Number -->
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
+          <label class="text-xs">Licence Number</label>
           <div class="relative mt-2 rounded-md shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
               <LocationIcon class="w-6 h-6" />
@@ -111,13 +88,13 @@ const register = () => {
 
             <input
               type="text"
-              v-model="company.country"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+              v-model="licenceNumber"
+              class="w-full px-12 py-2 border-transparent rounded-md shadow-md appearance-none input"
             />
           </div>
         </div>
         <!-- Max Employees Allowed -->
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
           <label class="text-xs">Max Employees Allowed</label>
           <div class="relative mt-2 rounded-md shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
@@ -126,13 +103,13 @@ const register = () => {
 
             <input
               type="number"
-              v-model="company.maxEmployeesAllowed"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+              v-model="maxEmployeesAllowed"
+              class="w-full px-12 py-2 border-transparent rounded-md shadow-md appearance-none input"
             />
           </div>
         </div>
 
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
           <label class="text-xs">Expiration Date</label>
           <div class="relative mt-2 rounded-md shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
@@ -140,20 +117,35 @@ const register = () => {
             </span>
 
             <input
-              type="date"
-              v-model="company.expirationDate"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+              type="text"
+              v-model="expirationDate"
+              class="w-full px-12 py-2 border-transparent rounded-md shadow-md appearance-none input"
+            />
+          </div>
+        </div>
+
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
+          <label class="text-xs">account Number</label>
+          <div class="relative mt-2 rounded-md shadow-sm">
+            <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
+              <CalendarIcon class="w-6 h-6" />
+            </span>
+
+            <input
+              type="text"
+              v-model="accountNumber"
+              class="w-full px-12 py-2 border-transparent rounded-md shadow-md appearance-none input"
             />
           </div>
         </div>
       </div>
-        <!-- Create Main Content -->
+      <!-- Create Main Content -->
       <div class="flex items-center justify-between px-5 py-3 text-gray-700 border-b">
         <h3 class="text-sm text-indigo-600">Create Main Content</h3>
       </div>
 
       <div class="flex flex-wrap">
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
           <label class="text-xs">first Name</label>
           <div class="relative mt-2 rounded-md shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
@@ -162,13 +154,13 @@ const register = () => {
 
             <input
               type="text"
-              v-model="company.firstName"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+              v-model="firstName"
+              class="w-full px-12 py-2 border-transparent shadow-md rounded-md appearance-none input"
             />
           </div>
         </div>
 
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
           <label class="text-xs">Last Name</label>
           <div class="relative mt-2 rounded-md shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
@@ -177,46 +169,26 @@ const register = () => {
 
             <input
               type="text"
-              v-model="company.lastName"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+              v-model="lastName"
+              class="w-full px-12 py-2 border-transparent rounded-md shadow-md appearance-none input"
             />
           </div>
         </div>
 
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
+        <div class="px-5 py-6 text-gray-700 bg-gray-50 border-b w-1/2">
           <label class="text-xs">Email</label>
           <div class="relative mt-2 rounded-md shadow-sm">
             <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
-              <MailIcon class="w-6 h-6" />
+              <EmailIcon class="w-6 h-6" />
             </span>
 
             <input
               type="email"
-              v-model="company.clientEmail"
-              class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
+              v-model="email"
+              class="w-full px-12 py-2 border-transparent rounded-md shadow-md appearance-none input"
             />
           </div>
         </div>
-        <!-- User Role -->
-        <div class="px-5 py-6 text-gray-700 bg-gray-200 border-b w-1/2">
-            <label class="text-xs">User Role</label>
-            <div class="relative mt-2 rounded-md shadow-sm">
-              <span class="absolute inset-y-0 left-0 flex items-center pl-3 text-gray-600">
-                <UserRole class="w-6 h-6" />
-              </span>
-
-              <select
-                name="UserRole"
-                v-model="company.userRole"
-                :v-bind="company.userRole"
-                class="w-full px-12 py-2 border-transparent rounded-md appearance-none focus:border-indigo-600 focus:ring focus:ring-opacity-40 focus:ring-indigo-500"
-              >
-                <option>Admin</option>
-                <option>Manager</option>
-                <option>Employee</option>
-              </select>
-            </div>
-          </div>
       </div>
       <!-- footer -->
       <div class="flex items-center justify-between px-5 py-3">
@@ -225,25 +197,13 @@ const register = () => {
             <input
               type="checkbox"
               name="sendRegistration"
-              v-model="company.sendRegistration"
+              v-model="sendRegistration"
               value="false"
-              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
+              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded"
             />
             <label for="vehicle1" class="ms-2 text-sm font-medium text-gray-900"
               >Send Registration !
             </label>
-          </div>
-          <div class="">
-            <input
-              type="checkbox"
-              name="sendWelcomeEmail"
-              v-model="company.isSpecialClient"
-              value="false"
-              class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 rounded focus:ring-blue-500"
-            />
-            <label for="link-checkbox" class="ms-2 text-sm font-medium text-gray-900"
-              >Special Client !</label
-            >
           </div>
         </div>
         <button
@@ -255,3 +215,15 @@ const register = () => {
     </form>
   </div>
 </template>
+
+<style scoped>
+.input {
+  border: solid #bbb 1px;
+}
+.input:focus {
+  border: solid #4f46e5 1px;
+  --tw-ring-color: rgb(99 102 241);
+  outline: none;
+  transition: all 0.3s ease;
+}
+</style>
